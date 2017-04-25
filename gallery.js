f3e390a55ab1c373a66abbb06875d608f7a9996a
Unknown <jhowell9@mail.csuchico.edu>
2017-04-25 14:27:26 -0700

    x24 (part 1) Done

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
+/* Jeremiah Howell */
+var chico=["Bear Hole","Covered Bridge","Upper Park","In N Out","Old 32"];
+var current;
 
-
-function shuffleGallery()
-{
-
+function shuffleGallery() {
+  var randomIndex;
+  do {
+  	randomIndex = Math.floor(Math.random() * chico.length);
+  }
+  while (randomIndex==current);
+	current = randomIndex;
+	
+  console.log(chico[current]);
 }
